f4f2b390e7cab1da0f01139507739ce23b572e8d
santiesleo <83185169+santiesleo@users.noreply.github.com>
2025-02-23 14:22:53 -0500

    fix: change route

--- src/main/java/co/analisys/entrenadores/controller/EntrenadoresController.java
+++ src/main/java/co/analisys/entrenadores/controller/EntrenadoresController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/entrenadores")
+@RequestMapping("api/entrenadores")
 class EntrenadoresController {
     private final IEntrenadorService entrenadorService;
 
